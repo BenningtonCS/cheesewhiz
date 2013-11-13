5030c79daa67bea9f29b8ac602f46b5802544471
WeedFox <tglenn@bennington.edu>
2013-11-13 13:13:32 -0500

    Comments and summary

--- listAdd.py
+++ listAdd.py
@@ -1,11 +1,36 @@
+#
+#			listAdd.py : an API for the entire cheeseWhiz family
+#			By Torrent Glenn
+#			11/13/2013
+#
+############################################################################################
+""" listAdd.py, located on backus in /data/listAdd, is the new interface for 
+managing what processes you want running and which you want dead. listAdd.py
+takes input with either a -r (run)or a -k (kill) option followed by a list 
+of processes. listAdd then resolves the intersection between hitlist.txt and
+whizzifest.txt by removing the shared processes from the file which the user
+was not adding to. i.e. if jankprocess.py is in whizzifest.txt running 
+"python listAdd.py -k jankprocess.py" will add jankprocess to the hitlist
+and remove it from the whizzifest. Hitman.py will kill processes on the 
+hitlist while readwhizzifest.py will run processes in the whizzifest. 
+As with any change to a file on backus, you MUST run mgen.py in order
+for the pis to know what's happening."""
+
+
+
 import os
 import sys 
 import whizconfig
 
+#remove the process name from the list for easier parsing
 del sys.argv[0]
+#create an empty string options in order to pick up any options
+# i.e. -k, -r
 options = ""
 
 def getList(listToGet):
+	#getList() parses a text file i.e. whizzifest or hitlist
+	# and then appends the contents to a list
 	newList = []
 	with open(whizconfig.path+listToGet,"r") as whizFile:
 		for line in whizFile:
@@ -13,40 +38,52 @@ def getList(listToGet):
 		return newList
 
 for item in sys.argv:
+	#here we parse the command line input and scoop up 
+	#anything that looks like an option (begins with "-")
 	if item[0] == "-":
+		#remove the items 
 		sys.argv.remove(item)
+		#and add them to the options string
 		options += item
 
-
+# in case some bozo tries to run and kill at the same time... we exit
 if "r" in options and "k" in options:
 	print "You need to choose whether to 'run' or 'kill'."
 	exit(0)
 
-
+#I had a simpler way to do this: with just "process = whizconfig.path + process"
+#but i didn't work so I did this complicated reference to index thing to add the 
+#appropriate path to each process
 for process in sys.argv:
 	sys.argv[sys.argv.index(process)] = whizconfig.path + sys.argv[sys.argv.index(process)]
-print "sys.argv : ", sys.argv
 
+# create and populate the toRun and toKill lists with the contents of the respective files
 toRun = []
 toKill = []
 toRun += getList("whizzifest.txt")
 toKill += getList("hitlist.txt")
 
-
+# if we have the "run" option selected 
+# add it (assuming it's not already there) to toRun
+#remove any instance of it from toKill
 if "r" in options:
 	for process in sys.argv:
 		if process in toKill:
 			toKill.remove(process)
 		if process not in toRun:
 			toRun.append(process)
-
+			
+# if we have the "kill" option selected 
+# add it (assuming it's not already there) to toKill
+#remove any instance of it from toRun
 elif "k" in options:
 	for process in sys.argv:
 		if process in toRun:
 			toRun.remove(process)
 		if process not in toKill:
 			toKill.append(process)
-
+			
+#now overwrite the previous text files with the new list.
 with open(whizconfig.path+"whizzifest.txt", "w") as whiz:
 	for item in toRun:
 		whiz.write(item+"\n")
